9df5516c3924b139211e14ef0d3d92a013e663d8
fukadashigeru <sakusaku721@gmail.com>
2020-09-29 15:20:32 +0900

    Initialize repository

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  [
+    {name: 'Jane Cooper', email: 'jane.cooper@example.com'},
+    {name: 'Cody Fisher', email: 'cody.fisher@example.com'},
+    {name: 'Esther Howard', email: 'esther.howard@example.com'},
+    {name: 'Jenny Wilson', email: 'jenny.wilson@example.com'},
+    {name: 'Kristin Watson', email: 'kristin.watson@example.com'},
+    {name: 'Cameron Williamson', email: 'cameron.williamson@example.com'}
+  ]
+)
